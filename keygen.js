1ff1167a36323df015b065470599e2e80f0a96b9
Tom <tomherring1999@gmail.com>
2019-06-24 18:11:10 +0100

    Implemented Address generation code into class. Add push of RSA PubKey to blockchain.

--- keygen.js
+++ keygen.js
@@ -15,7 +15,7 @@ console.log("ECDSA Private Key: " + ECDSA_PRIVATE_KEY);
 console.log("\n" + RSA_KEY.exportKey("public"));
 console.log("\n" + RSA_KEY.exportKey("private"));
 
-var data_to_encrypt = "hello 1234";
+var data_to_encrypt = "hello";
 
 var encrypted_data = RSA_KEY.encrypt(data_to_encrypt, "base64");
 console.log("\nEncrypted data:\n" + encrypted_data);
@@ -23,5 +23,41 @@ console.log("\nEncrypted data:\n" + encrypted_data);
 var decrypted_data = RSA_KEY.decrypt(encrypted_data);
 console.log("\nDecrypted data:\n" + decrypted_data);
 
-var fail_decrypt = RSA_KEY2.decrypt(encrypted_data);
-console.log("\nFail Decrypt:\n" + fail_decrypt);
+// var fail_decrypt = RSA_KEY2.decrypt(encrypted_data);
+// console.log("\nFail Decrypt:\n" + fail_decrypt);
+
+//Generates the Mail Account (Address).
+class Address {
+    constructor() {
+        this.ecdsa_key_pair = this.generateECDSAKeyPair();
+        this.rsa_key_pair = this.generateRSAKeyPair();
+    }
+
+    generateECDSAKeyPair() {
+        return EC_INSTANCE.genKeyPair();
+    }
+
+    getECDSAPublicKey() {
+        return this.ecdsa_key_pair.getPublic("hex");
+    }
+
+    getECDSAPrivateKey() {
+        return this.ecdsa_key_pair.getPrivate("hex");
+    }
+
+    generateRSAKeyPair() {
+        return new NODE_RSA({b: 2048});
+    }
+
+    getRSAPublicKey() {
+        return this.rsa_key_pair.exportKey("public");
+    }
+
+    getRSAPrivateKey() {
+        return this.rsa_key_pair.exportKey("private");
+    }
+
+    publishPublicKey() {
+        let pub_key_transaction = new Mail(this.getECDSAPublicKey(), "0x0", this.getRSAPublicKey());
+    }
+}
