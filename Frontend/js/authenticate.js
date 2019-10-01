849b4ec787202a5cf5c8e36530d7867cfa11e100
laustz <tomherring1999@gmail.com>
2019-10-01 22:29:12 +0100

    Set up more input validation on login page. Work in progress.

--- Frontend/js/authenticate.js
+++ Frontend/js/authenticate.js
@@ -1,47 +1,32 @@
 //Handle initial "Authenticate" button press.
 function authenticate() {
     //Get values of login form fields into variables.
-    var ecdsa_public_text = document.getElementById("ecdsa_public").value;
-    var ecdsa_private_text = document.getElementById("ecdsa_private").value;
-    var ecdsa_public_file = document.getElementById('ecdsa_public_file').files[0];
-    var ecdsa_private_file = document.getElementById('ecdsa_private_file').files[0];
+    var ecdsa_public_text = document.getElementById("ecdsa-public").value;
+    var ecdsa_private_text = document.getElementById("ecdsa-private").value;
 
-    checkECDSAKey(ecdsa_public_text, ecdsa_public_file, true);
-    checkECDSAKey(ecdsa_private_text, ecdsa_private_file, false);
+    var is_public_file = checkECDSAKey(ecdsa_public_text, true);
+    var is_private_file = checkECDSAKey(ecdsa_private_text, false);
 }
 
 //Evaluate inputs of the form and outline in red if incorrect, and display alert at bottom of form.
 function checkECDSAKey(ecdsa_text, ecdsa_file, is_public) {
+    //Set up ID targets.
+    id = "ecdsa-public"; //If Public.
+    id_label = "ecdsa-public-file-label"
+    if (!is_public) {
+        id = "ecdsa-private"; //If Private.
+        id_label = "ecdsa-private-file-label"
+    }
     if (ecdsa_text == "" && ecdsa_file == undefined) {
-        document.getElementById(is_public ? "ecdsa_public" : "ecdsa_private").setAttribute("style", "border: 2px solid red !important");
-        document.getElementById(is_public ? "ecdsa_public_file_label" : "ecdsa_private_file_label").setAttribute("style", "border: 2px solid red !important");
+        document.getElementById(id).setAttribute("style", "border: 2px solid red !important;");
+        document.getElementById(id_label).setAttribute("style", "border: 2px solid red !important;");
         document.getElementById("login-box-warning").innerHTML =
             "<div id='login-warning' class='alert alert-danger' role='alert'> \
                 You must enter or upload an ECDSA " + (is_public ? "Public" : "Private") + " Key to authenticate with. \
             </div>";
     }
     else {
-        document.getElementById(is_public ? "ecdsa_public" : "ecdsa_private").setAttribute("style", "border: 1px solid #ced4da !important");
-        document.getElementById(is_public ? "ecdsa_public_file_label" : "ecdsa_private_file_label").setAttribute("style", "border: 0 !important");
-        if (ecdsa_public_file != undefined) {
-            return true //
-        }
-        else {
-            return false //
-        }
+        document.getElementById(id).setAttribute("style", "border: 1px solid #ced4da !important;");
+        document.getElementById(id_label).setAttribute("style", "border: 0 !important;");
     }
-}
-
-function fetchECDSAKey(ecdsa_key, is_file, is_public) {
-    if (is_file) {
-        var reader = new FileReader();
-        reader.onload = function (event) {
-            return event.target.result;
-        }
-        reader.onerror = error => reject(error);
-        reader.readAsText(ecdsa_key);
-
-        console.log(reader.onload);
-    }
-
 }
---
+++ Frontend/js/upload_keys.js
@@ -0,0 +1,35 @@
+//Wait for file upload. If file uploaded, place into text area, and grey out upload button.
+document.getElementById("ecdsa-public-file").onchange = function () {
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        document.getElementById("ecdsa-public").value = event.target.result;
+    }
+    reader.onerror = error => reject(error);
+    reader.readAsText(document.getElementById("ecdsa-public-file").files[0]);
+
+    document.getElementById("ecdsa-public-file").disabled = true;
+    document.getElementById("ecdsa-public-file-label").setAttribute("style", "display: none;");
+    document.getElementById("clear-public-label").setAttribute("style", "display: inline;");
+
+};
+
+//Same as above but for Private Key.
+document.getElementById("ecdsa-private-file").onchange = function () {
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        document.getElementById("ecdsa-private").value = event.target.result;
+    }
+    reader.onerror = error => reject(error);
+    reader.readAsText(document.getElementById("ecdsa-private-file").files[0]);
+
+    document.getElementById("ecdsa-private-file").disabled = true;
+    document.getElementById("ecdsa-private-file-label").setAttribute("style", "display: none;");
+    document.getElementById("clear-private-label").setAttribute("style", "display: block;");
+};
+
+function clearFile(is_public) {
+    document.getElementById(is_public ? "ecdsa-public" : "ecdsa-private").value = "";
+    document.getElementById(is_public ? "ecdsa-public-file" : "ecdsa-private-file").value = "";
+    document.getElementById(is_public ? "clear-public-label" : "clear-private-label").setAttribute("style", "display: none;");
+    document.getElementById(is_public ? "ecdsa-public-file-label" : "ecdsa-private-file-label").setAttribute("style", "display: block;");
+}
