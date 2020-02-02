0280365fb217a14e9f18291b129347a7519fc3f0
tdherring <tomherring1999@gmail.com>
2020-02-02 15:38:26 +0000

    Key generation and download of keys implemented

--- Frontend/js/gen-keys.js
+++ Frontend/js/gen-keys.js
@@ -3,24 +3,43 @@ const EC_INSTANCE = new EC("secp256k1");
 const NODE_RSA = require("node-rsa");
 
 $("#gen-keys-btn").click(function () {
+    $("#keys").empty(); // Clear current key-gen if exists.
     genECDSA();
     genRSA();
 });
 
 function genECDSA() {
-    const ECDSA_KEY_PAIR = EC_INSTANCE.genKeyPair();
-    const ECDSA_PUBLIC_KEY = ECDSA_KEY_PAIR.getPublic("hex");
-    const ECDSA_PRIVATE_KEY = ECDSA_KEY_PAIR.getPrivate("hex");
+    let ecdsa_key_pair = EC_INSTANCE.genKeyPair(); // Generate ECDSA SECP256K1 key pair.
+    let ecdsa_public_key = ecdsa_key_pair.getPublic("hex"); //Extract public key.
+    let ecdsa_private_key = ecdsa_key_pair.getPrivate("hex"); //Extract private key.
 
-    console.log("ECDSA Public Key: " + ECDSA_PUBLIC_KEY);
-    console.log("ECDSA Private Key: " + ECDSA_PRIVATE_KEY);
+    $("#keys").append("<br><hr><h6>Wallet Address</h6>" + ecdsa_public_key + "<button class='btn btn-lg btn-primary btn-block text-uppercase download-btn'>Download</button>")
+    $("#download-ecdsa-public").click(function () { //Wait for download click.
+        downloadFile(ecdsa_public_key, "wallet.addr");
+    });
+
+    $("#keys").append("<h6>Wallet Password</h6>" + ecdsa_private_key + "<button class='btn btn-lg btn-primary btn-block text-uppercase download-btn'>Download</button>")
+    $("#download-ecdsa-private").click(function () {
+        downloadFile(ecdsa_private_key, "wallet.pass")
+    });
 }
 
 function genRSA() {
-    const RSA_KEY = new NODE_RSA({
+    let rsa_key = new NODE_RSA({ // Generate RSA key pair with 2048 bit key.
         b: 2048
     });
+    let rsa_public_key = rsa_key.exportKey("public");
+    let rsa_private_key = rsa_key.exportKey("private");
 
-    console.log("RSA Public Key: " + RSA_KEY.exportKey("public"));
-    console.log("RSA Private Key: " + RSA_KEY.exportKey("private"));
+    $("#keys").append("<h6>Mail Decryption Key</h6>" + rsa_private_key + "<button class='btn btn-lg btn-primary btn-block text-uppercase download-btn'>Download</button>")
+    $("#download-rsa-private").click(function () {
+        downloadFile(rsa_private_key, "private.key")
+    });
+}
+
+function downloadFile(data, name) {
+    let a = document.createElement('a');
+    a.href = "data:application/octet-stream," + encodeURIComponent(data);
+    a.download = name;
+    a.click();
 }
